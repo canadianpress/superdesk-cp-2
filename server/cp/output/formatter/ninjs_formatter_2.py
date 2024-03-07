6b56160ae84cdea9e1750f12da31a05046fde781
tcp-bhargav <115644367+tcp-bhargav@users.noreply.github.com>
2024-03-07 14:28:31 -0500

    Add files via upload
    Black Formatting done.

--- server/cp/output/formatter/ninjs_formatter_2.py
+++ server/cp/output/formatter/ninjs_formatter_2.py
@@ -648,8 +648,7 @@ class NINJSFormatter_2(Formatter):
                     **subject,
                     "name": (
                         subject["name"].lower()
-                        if subject.get("scheme")
-                        == "subject_custom"
+                        if subject.get("scheme") == "subject_custom"
                         and subject["name"] not in capital_subjects
                         else subject["name"]
                     ),
