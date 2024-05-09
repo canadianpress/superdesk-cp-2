f1871585c5f67b054ff0d11217c12012febddd2e
cmuldur <clm@cp.org>
2024-05-09 14:25:11 -0400

    Localize placeholder and improve French translations
    Internationalized the search placeholder in the auto-tagger to support
    different languages. Updated French Canadian translations for
    terminology consistency

--- client/extensions/auto-tagger/src/auto-tagging.tsx
+++ client/extensions/auto-tagger/src/auto-tagging.tsx
@@ -523,7 +523,7 @@ export function getAutoTaggingComponent(superdesk: ISuperdesk, label: string): I
                                                     key={this.state.forceRenderKey}
                                                     keyValue="keyValue"
                                                     items={[]}
-                                                    placeholder="Search for an entity or subject"
+                                                    placeholder={gettext('Search for an entity or subject')}
                                                     search={(searchString, callback) => {
                                                         let cancelled = false;
 
