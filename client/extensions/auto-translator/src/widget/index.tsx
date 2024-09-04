5fda1c1cee3b9d2876691f93f251ced1d778c013
txlcp <taylor.li@thecanadianpress.com>
2024-09-04 16:49:21 -0400

    initial mockup

--- client/extensions/auto-translator/src/widget/index.tsx
+++ client/extensions/auto-translator/src/widget/index.tsx
@@ -1,69 +1,162 @@
-import * as React from "react"
-import { superdesk } from "../superdesk"
+import * as React from "react";
+import { createPortal } from "react-dom";
+import { superdesk } from "../superdesk";
 import { IArticleSideWidgetComponentType } from "superdesk-api";
-import { IllustrationButton, Modal, SvgIconIllustration, Spacer, ResizablePanels } from "superdesk-ui-framework/react";
+import {
+  IllustrationButton,
+  Modal,
+  SvgIconIllustration,
+  Spacer,
+  ResizablePanels,
+  ButtonGroup,
+  Button,
+  Input,
+  Select,
+  Option,
+  Container,
+  ContentDivider,
+  GridList,
+} from "superdesk-ui-framework/react";
 
 const { AuthoringWidgetLayout, AuthoringWidgetHeading } = superdesk.components;
 
-const WIDGET_ID = 'auto-translator-widget'
+const WIDGET_ID = "auto-translator-widget";
 
-function renderResult({ header, body, footer }: { header?: JSX.Element, body: JSX.Element, footer?: JSX.Element }) {
-    return (
-        <AuthoringWidgetLayout
-            header={(
-                <Spacer v gap="0" alignItems="center">
-                    <AuthoringWidgetHeading
-                        widgetId={WIDGET_ID}
-                        // TODO: add localization
-                        widgetName={"Auto Translate"}
-                        editMode={false}
-                    />
-                    {header}
-                </Spacer>
-            )}
-            body={body}
-            footer={footer}
-        />
-    );
+function renderResult({ header, body, footer }: { header?: JSX.Element; body: JSX.Element; footer?: JSX.Element }) {
+  return (
+    <AuthoringWidgetLayout
+      header={
+        <Spacer v gap="0" alignItems="center">
+          <AuthoringWidgetHeading
+            widgetId={WIDGET_ID}
+            // TODO: add localization
+            widgetName={"Auto Translate"}
+            editMode={false}
+          />
+          {header}
+        </Spacer>
+      }
+      body={body}
+      footer={footer}
+    />
+  );
 }
 
 export class AutoTranslatorWidget extends React.Component<IArticleSideWidgetComponentType> {
-    state = { isTranslationOpen: false }
+  state = { isTranslationOpen: false };
 
-    render() {
-        console.count("autoTranslatorWidget")
-        return <>
-            {renderResult({
-                body: <div className="
-                                        sd-grid-list
-                                        sd-grid-list--xx-small
-                                        sd-grid-list--gap-s
-                                        sd-grid-list--no-margin
-                                    "
-                >
-                    <IllustrationButton
-                        text='Translate'
-                        onClick={() => {
-                            this.setState({ isTranslationOpen: true })
-                        }}
-                    >
-                        <SvgIconIllustration illustration="translate" />
-                    </IllustrationButton>
-                </div>
-            })}
-            {this.state.isTranslationOpen &&
-                <Modal headerTemplate="Extra large modal" visible={this.state.isTranslationOpen} size='x-large' onHide={() => { this.setState({ isTranslationOpen: false }) }} zIndex={10000}>
-                    <ResizablePanels direction="horizontal" primarySize={{ min: 33, default: 50 }} secondarySize={{ min: 33, default: 50 }}>
-                        <div>
-                            TEST LEFT SIDE
-                        </div>
-
-                        <div>
-                            TEST RIGHT SIDE
-                        </div>
-                    </ResizablePanels>
-                </Modal>
-            }
-        </>
-    }
+  render() {
+    return (
+      <>
+        {renderResult({
+          body: (
+            <div
+              className="
+                            sd-grid-list
+                            sd-grid-list--xx-small
+                            sd-grid-list--gap-s
+                            sd-grid-list--no-margin
+                        "
+            >
+              <IllustrationButton
+                text="Translate"
+                onClick={() => {
+                  this.setState({ isTranslationOpen: true });
+                }}
+              >
+                <SvgIconIllustration illustration="translate" />
+              </IllustrationButton>
+            </div>
+          ),
+        })}
+        {this.state.isTranslationOpen &&
+          createPortal(
+            <Modal
+              headerTemplate="Translate"
+              visible={this.state.isTranslationOpen}
+              size="x-large"
+              onHide={() => {
+                this.setState({ isTranslationOpen: false });
+              }}
+              footerTemplate={
+                <ButtonGroup align="end">
+                  <Button
+                    text="Cancel"
+                    style="hollow"
+                    onClick={() => {
+                      this.setState({ isTranslationOpen: false });
+                    }}
+                  />
+                  <Button text="Create" type="primary" onClick={() => {}} />
+                </ButtonGroup>
+              }
+            >
+              <>
+                <GridList>
+                  <Select value="Option 1" label="Select 1" onChange={() => {}}>
+                    <Option>Option 1</Option>
+                    <Option>Option 2</Option>
+                  </Select>
+                  <Select value="Option 2" label="Select 2" onChange={() => {}}>
+                    <Option>Option 1</Option>
+                    <Option>Option 2</Option>
+                  </Select>
+                  <Button text="Translate" type="primary" onClick={() => {}} />
+                </GridList>
+                <ContentDivider />
+                <ResizablePanels direction="horizontal" primarySize={{ min: 33, default: 50 }} secondarySize={{ min: 33, default: 50 }}>
+                  <Container gap="large" direction="column" className="sd-padding-2">
+                    <Input
+                      label="Headline"
+                      value={""}
+                      boxedStyle={true}
+                      boxedLable={true}
+                      maxLength={25}
+                      type="text"
+                      tabindex={0}
+                      onChange={() => {}}
+                    />
+                    <Input
+                      label="Extended Headline"
+                      value={""}
+                      boxedStyle={true}
+                      boxedLable={true}
+                      maxLength={25}
+                      type="text"
+                      tabindex={0}
+                      onChange={() => {}}
+                    />
+                    <Input label="Body" value={""} boxedStyle={true} boxedLable={true} maxLength={25} type="text" tabindex={0} onChange={() => {}} />
+                  </Container>
+                  <Container gap="large" direction="column" className="sd-padding-2">
+                    <Input
+                      label="Headline"
+                      value={""}
+                      boxedStyle={true}
+                      boxedLable={true}
+                      maxLength={25}
+                      type="text"
+                      tabindex={0}
+                      onChange={() => {}}
+                    />
+                    <Input
+                      label="Extended Headline"
+                      value={""}
+                      boxedStyle={true}
+                      boxedLable={true}
+                      maxLength={25}
+                      type="text"
+                      tabindex={0}
+                      onChange={() => {}}
+                    />
+                    <Input label="Body" value={""} boxedStyle={true} boxedLable={true} maxLength={25} type="text" tabindex={0} onChange={() => {}} />
+                  </Container>
+                </ResizablePanels>
+              </>
+            </Modal>,
+            document.body
+          )}
+      </>
+    );
+  }
 }
