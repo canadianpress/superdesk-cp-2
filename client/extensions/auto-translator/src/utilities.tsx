d112c0c37e12b3b9c0999e068bc4e27ad7433baf
txl-cp <taylor.li@thecanadianpress.com>
2024-10-24 13:54:27 -0400

    add translate api

--- client/extensions/auto-translator/src/utilities.tsx
+++ client/extensions/auto-translator/src/utilities.tsx
@@ -1,6 +1,8 @@
 export const WIDGET_ID = "auto-translator-widget" as const;
 
 export const TRANSLATION_METHODS = {
+  basic: "Google Basic",
+  advanced_nmt: "Google NMT",
+  advanced_llm: "Google LLM",
   deepl: "DeepL",
-  google: "Google",
 } as const;
