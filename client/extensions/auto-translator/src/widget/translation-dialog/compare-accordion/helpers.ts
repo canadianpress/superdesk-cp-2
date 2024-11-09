920f69214fd29b2239d2b326bd85647d85e8560d
txl-cp <taylor.li@thecanadianpress.com>
2024-11-09 10:26:22 -0500

    add sanitize and custom pretty diff

--- client/extensions/auto-translator/src/widget/translation-dialog/compare-accordion/helpers.ts
+++ client/extensions/auto-translator/src/widget/translation-dialog/compare-accordion/helpers.ts
@@ -4,74 +4,26 @@ import diff_match_patch, {
   DIFF_INSERT,
 } from "diff-match-patch";
 import DOMPurify from "dompurify";
-import { ISuperdesk } from "superdesk-api";
 
-export const sanitizeHtml = (html: string | Node) => DOMPurify.sanitize(html);
+export const sanitizeHtml = (html: string | Node) => {
+  const clean = DOMPurify.sanitize(html);
 
-export const getPrettyDiffHtml = ({
-  diffs,
-  gettext,
-}: {
-  diffs: diff_match_patch.Diff[];
-  gettext: ISuperdesk["localization"]["gettext"];
-}) => {
+  return clean;
+};
+
+export const getPrettyDiffHtml = (diffs: diff_match_patch.Diff[]) => {
   let html = [];
-  const patternPara = /\n/g;
-  const tempDiv = document.createElement("div");
 
-  const getFormattedElements = (text: string, type: "ins" | "del") => {
-    let elements = "";
-    const fragment = document.createRange().createContextualFragment(text);
-
-    fragment.childNodes.forEach((node) => {
-      if (node.nodeType === Node.ELEMENT_NODE) {
-        const element = node as Element;
-        const tag = element.nodeName.toLowerCase();
-
-        if (element.innerHTML)
-          elements += `<${tag}>${
-            type === "ins"
-              ? `<ins style="background:#e6ffe6;" aria-label=${gettext(
-                  "Inserted"
-                )}>${element.innerHTML}</ins>`
-              : `<del style="background:#ffe6e6;" aria-label=${gettext(
-                  "Deleted"
-                )}>${element.innerHTML}</del>`
-          }</${tag}>`;
-      } else if (node.nodeType === Node.TEXT_NODE) {
-        const element = node as Text;
-
-        if (element.textContent)
-          elements +=
-            type === "ins"
-              ? `<ins style="background:#e6ffe6;" aria-label=${gettext(
-                  "Inserted"
-                )}>${element.textContent}</ins>`
-              : `<del style="background:#ffe6e6;" aria-label=${gettext(
-                  "Deleted"
-                )}>${element.textContent}</del>`;
-      }
-    });
-
-    return elements;
-  };
-
-  for (let x = 0; x < diffs.length; x++) {
+  for (var x = 0; x < diffs.length; x++) {
     let op = diffs[x][0];
     let data = diffs[x][1];
-    let text = data.replace(patternPara, "");
-    tempDiv.innerHTML = text;
-
+    let text = data;
     switch (op) {
       case DIFF_INSERT:
-        html[x] = tempDiv.hasChildNodes()
-          ? getFormattedElements(text, "ins")
-          : `<ins style="background:#e6ffe6;">${text}</ins>`;
+        html[x] = '<ins style="background:#e6ffe6;">' + text + "</ins>";
         break;
       case DIFF_DELETE:
-        html[x] = tempDiv.hasChildNodes()
-          ? getFormattedElements(text, "del")
-          : `<del style="background:#ffe6e6;">${text}</del>`;
+        html[x] = '<del style="background:#ffe6e6;">' + text + "</del>";
         break;
       case DIFF_EQUAL:
         html[x] = text;
