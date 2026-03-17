4fdee74db6e7f451efd12646c95df39107b43380
Taylor Li <t4ylor.li@gmail.com>
2026-03-17 13:57:43 -0400

    add loader

--- client/extensions/research-tool/src/markdown-item.tsx
+++ client/extensions/research-tool/src/markdown-item.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
 import ReactMarkdown from "react-markdown";
-import { ButtonGroup, IconButton } from "superdesk-ui-framework/react";
+import { ButtonGroup, IconButton, Loader } from "superdesk-ui-framework/react";
+import { CitationTooltip } from "./citation-tooltip";
 import type { Markdown } from "./context/markdown-context";
 import { superdesk } from "./superdesk";
-import { CitationTooltip } from "./citation-tooltip";
 
-export const MarkdownItem = ({ markdown }: { markdown: Markdown }) => (
-  <>
-    <ReactMarkdown components={{ a: CitationTooltip }}>
-      {markdown.value}
-    </ReactMarkdown>
-    <ButtonGroup>
-      <IconButton
-        icon="copy"
-        ariaValue={superdesk.localization.gettext("Copy")}
-        onClick={() => {
-          navigator.clipboard.writeText(markdown.value);
-        }}
-      />
-    </ButtonGroup>
-  </>
-);
+export const MarkdownItem = ({ markdown }: { markdown: Markdown }) =>
+  !markdown.value ? (
+    <Loader
+      overlay={false}
+      width="fit-content"
+      height="fit-content"
+      backgroundColor="transparent"
+    />
+  ) : (
+    <>
+      <ReactMarkdown components={{ a: CitationTooltip }}>
+        {markdown.value}
+      </ReactMarkdown>
+      <ButtonGroup>
+        <IconButton
+          icon="copy"
+          ariaValue={superdesk.localization.gettext("Copy")}
+          onClick={() => {
+            navigator.clipboard.writeText(markdown.value);
+          }}
+        />
+      </ButtonGroup>
+    </>
+  );
